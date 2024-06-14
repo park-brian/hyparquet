645d8b21273331855e10c4b2ce0c84fb94756468
Brian Park <park-brian@users.noreply.github.com>
2024-06-14 19:58:26 -0400

    move helpers

--- test/helpers.js
+++ test/helpers.js
@@ -1,5 +1,4 @@
 import fs from 'fs'
-
 /**
  * Helper function to read .parquet file into ArrayBuffer
  *
@@ -25,6 +24,132 @@ export function fileToAsyncBuffer(filePath) {
 }
 
 /**
+ * Wrap a very large .parquet file in an AsyncBuffer
+ *
+ * @param {string} filePath
+ * @returns {import('../src/types.js').AsyncBuffer}
+ */
+export function largeFileToAsyncBuffer(filePath) {
+  return {
+    byteLength: fs.statSync(filePath).size,
+    slice: async (start, end) => {
+      const fileHandle = await fs.promises.open(filePath, 'r')
+      const length = end - start
+      const buffer = new Uint8Array(length)
+      await fileHandle.read(buffer, 0, length, start)
+      await fileHandle.close()
+      return buffer.buffer
+    },
+    sliceBatch: async (ranges) => {
+      const fileHandle = await fs.promises.open(filePath, 'r')
+      const buffers = []
+      for (const range of ranges) {
+        if (!range) {
+          buffers.push(new ArrayBuffer(0))
+          continue
+        }
+        const [start, end] = range
+        const length = end - start
+        const buffer = new Uint8Array(length)
+        await fileHandle.read(buffer, 0, length, start)
+        buffers.push(buffer.buffer)
+      }
+      await fileHandle.close()
+      return buffers
+    },
+  }
+}
+
+export async function getLength(url) {
+  const response = await fetch(url, { method: 'HEAD', cache: 'no-store' })
+  return parseInt(response.headers.get('Content-Length'))
+}
+
+export async function getBytes(url, start, end) {
+  const response = await fetch(url, {
+    headers: { Range: `bytes=${start}-${end - 1}` },
+    cache: 'no-store',
+  })
+  return await response.arrayBuffer()
+}
+
+export async function remoteFileToAsyncBuffer(url) {
+  return {
+    byteLength: await getLength(url),
+    slice: async (start, end) => {
+      return await getBytes(url, start, end)
+    },
+    sliceBatch: async (ranges) => {
+      const requests = chunk(ranges, 100).map(ranges => fetchBatchedRanges(url, ranges))
+      const results = await Promise.all(requests)
+      return results.flat()
+    },
+  }
+}
+
+function chunk(list, size) {
+  return Array.from({ length: Math.ceil(list.length / size) }, (_, i) =>
+    list.slice(i * size, i * size + size)
+  )
+}
+
+/**
+ * Returns an ArrayBuffers from a multirange request
+ *
+ * @param {string} url
+ * @param {(null | [number, number])[]} ranges
+ * @returns {Promise<ArrayBuffer[]>}
+ */
+export async function fetchBatchedRanges(url, ranges) {
+  const arrayBuffers = new Array(ranges.length).fill(null).map(_ => new ArrayBuffer(0))
+  if (!ranges.filter(Boolean).length) return arrayBuffers
+
+  const headers = { range: 'bytes=' + ranges.filter(Boolean).map(([start, end]) => [start, end - 1].join('-')).join(',') }
+  const getRangeIndex = (ranges, contentRange) => {
+    const [_, start, end] = contentRange?.match(/bytes (\d+)-(\d+)\/(\d+)/)
+    return ranges.findIndex(r => r && r[0] === parseInt(start) && r[1] === parseInt(end) + 1)
+  }
+
+  const response = await fetch(url, { headers, cache: 'no-store' })
+  const contentType = response.headers.get('content-type')
+
+  if (contentType?.includes('application/octet-stream')) {
+    const contentRange = response.headers.get('content-range')
+    const rangeIndex = getRangeIndex(ranges, contentRange)
+    arrayBuffers[rangeIndex] = await response.arrayBuffer()
+    return arrayBuffers
+  }
+
+  else if (contentType?.includes('multipart/byteranges')) {
+    const boundary = '--' + contentType?.split('boundary=')[1]
+    const responseArrayBuffer = await response.arrayBuffer()
+    const responseText = new TextDecoder('ascii', { fatal: true }).decode(responseArrayBuffer) // use ascii since it maps nicely to Uint8Array
+    const parts = responseText
+      .split(boundary)
+      .filter(part => !['', '--'].includes(part.trim()))
+      .map(part => part.split('\r\n\r\n').map(p => p.replace(/\r\n$/, '')))
+
+    let offset = 0
+    for (const [header, body] of parts) {
+      const parsedHeaders = header.split('\r\n').filter(Boolean).reduce((acc, line) => {
+        const [key, value] = line.split(': ')
+        return { ...acc, [key.toLowerCase()]: value }
+      }, {})
+
+      const bodyStartIndex = responseText.indexOf(body, offset)
+      offset = bodyStartIndex + body.length
+      const bodyArrayBuffer = responseArrayBuffer.slice(bodyStartIndex, offset)
+
+      const rangeIndex = getRangeIndex(ranges, parsedHeaders['content-range'])
+      arrayBuffers[rangeIndex] = bodyArrayBuffer
+    }
+  }
+
+  return arrayBuffers
+}
+
+
+/**
  * Read .parquet file into JSON
  *
  * @param {string} filePath
@@ -43,4 +168,26 @@ export function fileToJson(filePath) {
  */
 export function reader(bytes) {
   return { view: new DataView(new Uint8Array(bytes).buffer), offset: 0 }
+}
+
+
+export function concatenateArrayBuffers(buffers) {
+  const bufferLength = buffers.reduce((acc, buffer) => acc + buffer.byteLength, 0)
+  const buffer = new ArrayBuffer(bufferLength)
+  const array = new Uint8Array(buffer)
+  let offset = 0
+  for (const buffer of buffers) {
+    array.set(new Uint8Array(buffer), offset)
+    offset += buffer.byteLength
+  }
+  return buffer
+}
+
+export function groupBy(array, predicate) {
+  return array.reduce((acc, el) => {
+    const key = predicate(el)
+    if (!acc[key]) acc[key] = []
+    acc[key].push(el)
+    return acc
+  }, {})
 }
